70d343eecd4ea692bf6e241713e311110d25b560
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-07-16 14:52:28 -0700

    set up file path

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+// app.post("/api/notes", (req, res) => {
+//     res.writeFile(path.join(__dirname, "/public/notes.html"));
+// });
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
