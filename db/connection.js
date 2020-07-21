e5bac56616b2c7d319172ba1b5fedc53cdc6f8af
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-07-21 09:45:35 -0700

    added connection to mysql

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const connection = require("connection");
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "notetaker_db"
+  });
+}
+
+module.exports = connection;
